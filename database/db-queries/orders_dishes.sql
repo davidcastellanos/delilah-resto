fe5bf3ed01fdb602ef6c84eb845efb9699f5711d
davidcastellanos <castellanos.johann@gmail.com>
2022-12-27 15:02:21 -0500

    [UPDATE] deleting table if exist

--- database/db-queries/orders_dishes.sql
+++ database/db-queries/orders_dishes.sql
@@ -20,3 +20,22 @@ USE `delilah_resto`;
 --
 -- Table structure for table `Orders_dishes`
 --
+
+DROP TABLE IF EXISTS `Orders_dishes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Orders_dishes` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Order_id` int DEFAULT NULL,
+  `Dish_id` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK=>Orders_dishes.Order_id-Orders.ID` (`Order_id`),
+  KEY `FK=>Orders_dishes.Dish_id-Dishes.ID` (`Dish_id`),
+  CONSTRAINT `FK=>Orders_dishes.Dish_id-Dishes.ID` FOREIGN KEY (`Dish_id`) REFERENCES `Dishes` (`ID`),
+  CONSTRAINT `FK=>Orders_dishes.Order_id-Orders.ID` FOREIGN KEY (`Order_id`) REFERENCES `Orders` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders_dishes`
+--
