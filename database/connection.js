63f39c159d08c16e893b54c622ba29faf2fd723b
davidcastellanos <castellanos.johann@gmail.com>
2022-12-27 15:09:21 -0500

    [UPDATE] sequelize implemented

--- database/connection.js
+++ database/connection.js
@@ -5,3 +5,14 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_NAME = process.env.DB_NAME;
+
+
+const path = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const sequelize = new Sequelize(path, { operatorsAliases: false });
+sequelize.authenticate().then(() => {
+    console.log("Connected to Database");
+}).catch(err => {
+    console.error("Connection not stablished", err);
+})
+
+module.exports = sequelize;
