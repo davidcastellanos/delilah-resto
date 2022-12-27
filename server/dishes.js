1b7840b55c64e0b25ac62a7d1f94cab6bb185086
davidcastellanos <castellanos.johann@gmail.com>
2022-12-27 15:12:34 -0500

    [UPDATE] verifying token error 4**

--- server/dishes.js
+++ server/dishes.js
@@ -20,3 +20,20 @@ router.use(function (err, req, res, next) {
         next(err);
     }
 })
+
+router.get("/dishes", async (req, res) => {
+    //Route to get all the dishes. For customers and admins
+    try {
+        const records = await sequelize.query(
+            "SELECT * FROM dishes", { type: sequelize.QueryTypes.SELECT }
+            )
+            if(records.length == 0) {
+                res.status(404).json("There's no data")
+            } else {
+                res.status(200).json(records);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(400);
+        }
+})
