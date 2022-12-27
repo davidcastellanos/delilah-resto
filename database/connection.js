2578e4859861ab3b180c6ca5835a8068dd0e4dcb
davidcastellanos <castellanos.johann@gmail.com>
2022-12-27 15:08:20 -0500

    [UPDATE] creating const using enviroment

---
+++ database/connection.js
@@ -0,0 +1,7 @@
+const Sequelize = require("sequelize");
+require("dotenv").config({ path: "../.env" });
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
